58451eb6c01a24bb8d0cfcecc6cb7193ac28f65e
keisas <hurisasi0818@gmail.com>
2023-07-17 12:39:30 +0900

    chapter-17

--- store/stoer.go
+++ store/stoer.go
@@ -1,34 +1,34 @@
-// package store
+package store
 
-// import (
-// 	"errors"
+import (
+	"errors"
 
-// 	"github.com/keisas/golang.git/entity"
-// )
+	"github.com/keisas/golang.git/entity"
+)
 
-// var (
-// 	Tasks = &TaskStore{Tasks: map[int]*entity.Task{}}
+var (
+	Tasks = &TaskStore{Tasks: map[entity.TaskID]*entity.Task{}}
 
-// 	ErrNotFound = errors.New("not found")
-// )
+	ErrNotFound = errors.New("not found")
+)
 
-// type TaskStore struct {
-// 	// 動作確認用の仮実装
-// 	LastID entity.TaskID
-// 	Tasks  map[entity.TaskID]*entity.Task
-// }
+type TaskStore struct {
+	// 動作確認用の仮実装なのであえてexportしている。
+	LastID entity.TaskID
+	Tasks  map[entity.TaskID]*entity.Task
+}
 
-// func (ts *TaskStore) Add(t *entity.Task) (int, error) {
-// 	ts.LastID++
-// 	t.ID = ts.LastID
-// 	ts.Tasks[t.ID] = t
-// 	return t.ID, nil
-// }
+func (ts *TaskStore) Add(t *entity.Task) (entity.TaskID, error) {
+	ts.LastID++
+	t.ID = ts.LastID
+	ts.Tasks[t.ID] = t
+	return t.ID, nil
+}
 
-// func (ts *TaskStore) All() entity.Tasks {
-// 	tasks := make([]*entity.Task, len(ts.Tasks))
-// 	for i, t := range ts.Tasks {
-// 		tasks[i-1] = t
-// 	}
-// 	return tasks
-// }
+func (ts *TaskStore) All() entity.Tasks {
+	tasks := make([]*entity.Task, len(ts.Tasks))
+	for i, t := range ts.Tasks {
+		tasks[i-1] = t
+	}
+	return tasks
+}
